2438a75567a4d518a04fb22189f68bc1b3d24edb
kp-zealous <110828621+kp-zealous@users.noreply.github.com>
2023-02-19 12:13:28 +0530

    Create maxelementstack.py

---
+++ maximum element in stack/maxelementstack.py
@@ -0,0 +1,30 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def push(self, data):
+        self.items.append(data)
+ 
+    def pop(self):
+        return self.items.pop()
+        
+    def prints(self):
+        return max(self.items)
+def getMax(operations):
+    stack = Stack()
+    m=[]
+    for i in operations:
+        i=i.split(" ")
+        if eval(i[0])==1:
+            stack.push(int(i[1]))
+        elif eval(i[0])==2:
+            stack.pop()
+        else:
+            m.append(stack.prints())
+    return m
+    
+    
+    This code is contributed by PR.KRITHIKA PRIYA
