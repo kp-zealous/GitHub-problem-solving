48903d4b1a655d7eec5231d98a46cd3916d5d102
kp-zealous <110828621+kp-zealous@users.noreply.github.com>
2023-02-20 10:56:29 +0530

    Create delete.py

---
+++ linked list/delete a node/delete.py
@@ -0,0 +1,72 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+
+def deleteNode(llist, position):
+    if position == 0:
+        return llist.next
+    if llist is None:
+        return llist
+    val=llist
+    val2=llist.next
+    for i in range(position-1):
+        val=val2
+        val2=val2.next
+        val.next=val2.next
+    return llist
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
